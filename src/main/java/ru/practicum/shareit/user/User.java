55e442f93ac94616c874095423af3c7e45454e61
Aleksandr Vasilyev <ap.vasilyev@yandex.ru>
2022-09-30 08:30:12 +0300

    feat: add item, user controllers

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,11 @@
 package ru.practicum.shareit.user;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.*;
+
+@Data
+@Builder
 public class User {
+    Long id;
+    String name;
+    String email;
 }
---
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -0,0 +1,23 @@
+package ru.practicum.shareit.user.dto;
+
+import lombok.*;
+import ru.practicum.shareit.util.Create;
+import ru.practicum.shareit.util.Update;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Builder
+public class UserDto {
+    Long id;
+
+    @NotNull(message = "name/login is required", groups = {Create.class})
+    @NotBlank(message = "name/login is required", groups = {Create.class})
+    String name;
+
+    @NotNull(message = "email is required", groups = {Create.class})
+    @Email(message = "email is invalid", groups = {Create.class, Update.class})
+    String email;
+}
